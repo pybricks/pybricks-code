7b78a45917daf7aa274dd3f5d57f51a678182f39
David Lechner <david@lechnology.com>
2020-04-07 23:17:39 -0500

    embed ace editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import AceEditor from "react-ace";
 import './App.css';
 
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-github";
+
+function onChange(newValue: string) {
+    console.log("change", newValue);
+  }
+  
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <AceEditor
+            mode="python"
+            theme="github"
+            onChange={onChange}
+            name="editor"
+            editorProps={{ $blockScrolling: true }}
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
