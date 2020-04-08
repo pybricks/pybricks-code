cdfd2249342766e3b74c4d17f422f1d500b324af
David Lechner <david@lechnology.com>
2020-04-07 23:34:33 -0500

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
 import React from 'react';
-import AceEditor from "react-ace";
+import AceEditor from 'react-ace';
 import './App.css';
 
-import "ace-builds/src-noconflict/mode-python";
-import "ace-builds/src-noconflict/theme-github";
+import 'ace-builds/src-noconflict/mode-python';
+import 'ace-builds/src-noconflict/theme-github';
 
 function onChange(newValue: string) {
-    console.log("change", newValue);
-  }
-  
+    console.log('change', newValue);
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <AceEditor
-            mode="python"
-            theme="github"
-            onChange={onChange}
-            name="editor"
-            editorProps={{ $blockScrolling: true }}
-        />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <AceEditor
+                    mode="python"
+                    theme="github"
+                    onChange={onChange}
+                    name="editor"
+                    editorProps={{ $blockScrolling: true }}
+                />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
