4668143b8394cbd9019304dd1e2cd88c68e0112b
David Lechner <david@pybricks.com>
2020-06-08 19:36:59 -0500

    refactor lookup function

--- src/components/button.test.ts
+++ src/components/button.test.ts
@@ -1,18 +1,9 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2020 The Pybricks Authors
 
+import { lookup } from '../utils';
 import { TooltipId } from './button';
 import en from './button.en.json';
-
-function lookup(obj: object, id: string): string | undefined {
-    const value = id
-        .split('.')
-        .reduce((pv, cv) => pv && (pv as Record<string, object>)[cv], obj);
-    if (typeof value === 'string') {
-        return value;
-    }
-    return undefined;
-}
 
 describe('Ensure .json file has matches for TooltipIds', () => {
     test.each(Object.values(TooltipId))('%s', (id) => {
--- src/components/editor.test.ts
+++ src/components/editor.test.ts
@@ -1,18 +1,9 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2020 The Pybricks Authors
 
+import { lookup } from '../utils';
 import { EditorStringId } from './editor';
 import en from './editor.en.json';
-
-function lookup(obj: object, id: string): string | undefined {
-    const value = id
-        .split('.')
-        .reduce((pv, cv) => pv && (pv as Record<string, object>)[cv], obj);
-    if (typeof value === 'string') {
-        return value;
-    }
-    return undefined;
-}
 
 describe('Ensure .json file has matches for EditorStringId', () => {
     test.each(Object.values(EditorStringId))('%s', (id) => {
