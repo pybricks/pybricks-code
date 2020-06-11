c3d4a07053e1784119b6e8dfc936667c85fe6623
David Lechner <david@pybricks.com>
2020-06-10 21:54:12 -0500

    fix TextEncoder/TextDecoder on CI
    was working locally but not on CI for some reason

--- test/env.js
+++ test/env.js
@@ -1,19 +1,18 @@
 const Environment = require('jest-environment-jsdom');
+const { TextEncoder, TextDecoder } = require('util')
 
 /**
- * A custom environment to set TextDecoder/TextEncoder
+ * A custom environment to set TextEncoder/TextDecoder
  * Thanks https://stackoverflow.com/a/57713960/1976323
  */
 module.exports = class CustomTestEnvironment extends Environment {
     async setup() {
         await super.setup();
-        if (typeof TextDecoder === 'undefined') {
-            const { TextDecoder } = require('util');
-            this.global.TextDecoder = TextDecoder;
-        }
-        if (typeof TextEncoder === 'undefined') {
-            const { TextEncoder } = require('util');
+        if (this.global.TextEncoder === undefined) {
             this.global.TextEncoder = TextEncoder;
+        }
+        if (this.global.TextDecoder === undefined) {
+            this.global.TextDecoder = TextDecoder;
         }
     }
 };
