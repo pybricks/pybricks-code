9551bdc7f091bb3fd92f75d2e6589015327e2f12
David Lechner <david@pybricks.com>
2021-01-22 10:28:04 -0600

    Pybricks firmware v3.0.0b1

--- craco.config.js
+++ craco.config.js
@@ -64,29 +64,6 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 USE OR OTHER DEALINGS IN THE SOFTWARE.`;
 }
 
-const pybricksLicense = `MIT License
-
-Copyright (c) 2018-2021 The Pybricks Authors
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-`;
-
 const shopifyLicense = `MIT License
 
 Copyright (c) 2021 Shopify
@@ -110,7 +87,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.`;
 
 const licenseTextOverrides = {
-    '@pybricks/firmware': pybricksLicense,
     '@shopify/dates': shopifyLicense,
     '@shopify/decorators': shopifyLicense,
     '@shopify/function-enhancers': shopifyLicense,
