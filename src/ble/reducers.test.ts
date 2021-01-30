d3e67950a6db5c21229aac00c58c2d3ef7b8be3a
David Lechner <david@pybricks.com>
2021-01-30 12:32:40 -0600

    add tests for ble reducers

---
+++ src/ble/reducers.test.ts
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2021 The Pybricks Authors
+
+import { Action } from '../actions';
+import {
+    BleDeviceDidFailToConnectReason,
+    connect,
+    didConnect,
+    didDisconnect,
+    didFailToConnect,
+    disconnect,
+} from './actions';
+import reducers, { BleConnectionState } from './reducers';
+
+type State = ReturnType<typeof reducers>;
+
+test('initial state', () => {
+    expect(reducers(undefined, {} as Action)).toMatchInlineSnapshot(`
+        Object {
+          "connection": "ble.connection.state.disconnected",
+        }
+    `);
+});
+
+test('connection', () => {
+    expect(
+        reducers({ connection: BleConnectionState.Disconnected } as State, connect())
+            .connection,
+    ).toBe(BleConnectionState.Connecting);
+    expect(
+        reducers({ connection: BleConnectionState.Connecting } as State, didConnect())
+            .connection,
+    ).toBe(BleConnectionState.Connected);
+    expect(
+        reducers(
+            { connection: BleConnectionState.Connecting } as State,
+            didFailToConnect({} as BleDeviceDidFailToConnectReason),
+        ).connection,
+    ).toBe(BleConnectionState.Disconnected);
+    expect(
+        reducers({ connection: BleConnectionState.Connected } as State, disconnect())
+            .connection,
+    ).toBe(BleConnectionState.Disconnecting);
+    expect(
+        reducers(
+            { connection: BleConnectionState.Disconnecting } as State,
+            didDisconnect(),
+        ).connection,
+    ).toBe(BleConnectionState.Disconnected);
+});
