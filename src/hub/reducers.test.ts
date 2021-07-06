pybricks/pybricks-code

commit 7a5aa7589443bc11d77ff62c91535407918e29ba

hub: fix bad hub runtime state when disconnected
This fixes a bad hub runtime state when the hub disconnects during a
download. This caused the run button in the UI to be enabled when the
hub was disconnected.

Issue: https://github.com/pybricks/support/issues/378

src/hub/reducers.ts
@@ -46,21 +46,38 @@ const runtime: Reducer<HubRuntimeState, Action> = (
         case BleDeviceActionType.DidDisconnect:
             return HubRuntimeState.Disconnected;
         case HubActionType.DidStartDownload:
+            // disconnected overrides download
+            if (state === HubRuntimeState.Disconnected) {
+                return state;
+            }
             return HubRuntimeState.Loading;
         case HubActionType.DidFinishDownload:
+            // disconnected overrides download
+            if (state === HubRuntimeState.Disconnected) {
+                return state;
+            }
             return HubRuntimeState.Loaded;
         case HubActionType.DidFailToFinishDownload:
+            // disconnected overrides download
+            if (state === HubRuntimeState.Disconnected) {
+                return state;
+            }
             return HubRuntimeState.Idle;
         case BlePybricksServiceEventActionType.StatusReport:
             // The loading state is determined solely by the IDE, so we can't
             // let the hub status interfere with it.
-            if (state !== HubRuntimeState.Loading) {
-                if (action.statusFlags & statusToFlag(Status.UserProgramRunning)) {
-                    return HubRuntimeState.Running;
-                }
-                return HubRuntimeState.Idle;
+            if (
+                state === HubRuntimeState.Disconnected ||
+                state === HubRuntimeState.Loading
+            ) {
+                return state;
             }
-            return state;
+
+            if (action.statusFlags & statusToFlag(Status.UserProgramRunning)) {
+                return HubRuntimeState.Running;
+            }
+
+            return HubRuntimeState.Idle;
         default:
             return state;
     }

src/hub/reducers.test.ts
@@ -1,0 +1,141 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2021 The Pybricks Authors
+
+import { Action } from '../actions';
+import { statusReportEvent } from '../ble-pybricks-service/actions';
+import { Status, statusToFlag } from '../ble-pybricks-service/protocol';
+import { didDisconnect } from '../ble/actions';
+import {
+    didFailToFinishDownload,
+    didFinishDownload,
+    didProgressDownload,
+    didStartDownload,
+} from './actions';
+import reducers, { HubRuntimeState } from './reducers';
+
+type State = ReturnType<typeof reducers>;
+
+test('initial state', () => {
+    expect(reducers(undefined, {} as Action)).toMatchInlineSnapshot(`
+        Object {
+          "downloadProgress": null,
+          "runtime": "hub.runtime.disconnected",
+        }
+    `);
+});
+
+describe('runtime', () => {
+    test.each(Object.values(HubRuntimeState))('didDisconnect', (startingState) => {
+        // all states are overridden by disconnect
+        expect(
+            reducers({ runtime: startingState } as State, didDisconnect()).runtime,
+        ).toBe(HubRuntimeState.Disconnected);
+    });
+
+    test('didStartDownload', () => {
+        // download ignored if disconnected
+        expect(
+            reducers(
+                { runtime: HubRuntimeState.Disconnected } as State,
+                didStartDownload(),
+            ).runtime,
+        ).toBe(HubRuntimeState.Disconnected);
+        expect(
+            reducers({ runtime: HubRuntimeState.Idle } as State, didStartDownload())
+                .runtime,
+        ).toBe(HubRuntimeState.Loading);
+    });
+
+    test('didProgressDownload', () => {
+        // download ignored if disconnected
+        expect(
+            reducers(
+                { runtime: HubRuntimeState.Disconnected } as State,
+                didProgressDownload(0),
+            ).runtime,
+        ).toBe(HubRuntimeState.Disconnected);
+
+        // this shouldn't have any effect
+        expect(
+            reducers(
+                { runtime: HubRuntimeState.Loading } as State,
+                didProgressDownload(0),
+            ).runtime,
+        ).toBe(HubRuntimeState.Loading);
+    });
+
+    test('didFinishDownload', () => {
+        // download ignored if disconnected
+        expect(
+            reducers(
+                { runtime: HubRuntimeState.Disconnected } as State,
+                didFinishDownload(),
+            ).runtime,
+        ).toBe(HubRuntimeState.Disconnected);
+
+        // normal operation
+        expect(
+            reducers({ runtime: HubRuntimeState.Loading } as State, didFinishDownload())
+                .runtime,
+        ).toBe(HubRuntimeState.Loaded);
+    });
+
+    test('didFinishDownload', () => {
+        // download ignored if disconnected
+        expect(
+            reducers(
+                { runtime: HubRuntimeState.Disconnected } as State,
+                didFailToFinishDownload(),
+            ).runtime,
+        ).toBe(HubRuntimeState.Disconnected);
+
+        // normal operation for error path
+        expect(
+            reducers(
+                { runtime: HubRuntimeState.Loading } as State,
+                didFailToFinishDownload(),
+            ).runtime,
+        ).toBe(HubRuntimeState.Idle);
+    });
+
+    test('statusReportEvent', () => {
+        // don't ever expect this to happen in practice since we can't receive
+        // updates while disconnected
+        expect(
+            reducers(
+                { runtime: HubRuntimeState.Disconnected } as State,
+                statusReportEvent(statusToFlag(Status.UserProgramRunning)),
+            ).runtime,
+        ).toBe(HubRuntimeState.Disconnected);
+
+        // status update ignored while download not finished
+        expect(
+            reducers(
+                { runtime: HubRuntimeState.Loading } as State,
+                statusReportEvent(statusToFlag(Status.UserProgramRunning)),
+            ).runtime,
+        ).toBe(HubRuntimeState.Loading);
+
+        // normal operation - user program started
+        expect(
+            reducers(
+                { runtime: HubRuntimeState.Loaded } as State,
+                statusReportEvent(statusToFlag(Status.UserProgramRunning)),
+            ).runtime,
+        ).toBe(HubRuntimeState.Running);
+
+        // really short program run finished before receiving download finished
+        expect(
+            reducers({ runtime: HubRuntimeState.Loaded } as State, statusReportEvent(0))
+                .runtime,
+        ).toBe(HubRuntimeState.Idle);
+
+        // normal operation - user program stopped
+        expect(
+            reducers(
+                { runtime: HubRuntimeState.Running } as State,
+                statusReportEvent(0),
+            ).runtime,
+        ).toBe(HubRuntimeState.Idle);
+    });
+});

commit b16f2c1ca9c9e7dbee4f590084c77d055714050e

hub: fix buttons not enabled after connection
A previous commit broke buttons after connecting since state all state
transitions were changed to be ignored if the current state was
"disconnected". This made it impossible to get into any other state.

This brings back use of the "unknown" state upon initial connection
to provide a path out of the "disconnected" state.

Fixes: 7a5aa7589443bc11d77ff62c91535407918e29ba

src/hub/reducers.ts
@@ -43,6 +43,8 @@ const runtime: Reducer<HubRuntimeState, Action> = (
     action,
 ) => {
     switch (action.type) {
+        case BleDeviceActionType.DidConnect:
+            return HubRuntimeState.Unknown;
         case BleDeviceActionType.DidDisconnect:
             return HubRuntimeState.Disconnected;
         case HubActionType.DidStartDownload:

src/hub/reducers.test.ts
@@ -4,7 +4,7 @@
 import { Action } from '../actions';
 import { statusReportEvent } from '../ble-pybricks-service/actions';
 import { Status, statusToFlag } from '../ble-pybricks-service/protocol';
-import { didDisconnect } from '../ble/actions';
+import { didConnect, didDisconnect } from '../ble/actions';
 import {
     didFailToFinishDownload,
     didFinishDownload,
@@ -25,6 +25,13 @@ test('initial state', () => {
 });
 
 describe('runtime', () => {
+    test('', () => {
+        expect(
+            reducers({ runtime: HubRuntimeState.Disconnected } as State, didConnect())
+                .runtime,
+        ).toBe(HubRuntimeState.Unknown);
+    });
+
     test.each(Object.values(HubRuntimeState))('didDisconnect', (startingState) => {
         // all states are overridden by disconnect
         expect(