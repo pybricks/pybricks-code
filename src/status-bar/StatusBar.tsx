5d201e61ae5ad109c7dd70f54395da0497648a87
David Lechner <david@pybricks.com>
2021-12-18 15:16:54 -0600

    status-bar: convert StatusBar to function

--- src/status-bar/StatusBar.tsx
+++ src/status-bar/StatusBar.tsx
@@ -2,21 +2,18 @@
 // Copyright (c) 2020-2021 The Pybricks Authors
 
 import React from 'react';
-import { connect } from 'react-redux';
 
 import './status-bar.scss';
 
-class StatusBar extends React.Component {
-    render(): JSX.Element {
-        return (
-            <div
-                className="pb-status-bar"
-                role="status"
-                aria-live="off"
-                onContextMenu={(e): void => e.preventDefault()}
-            ></div>
-        );
-    }
-}
+const StatusBar: React.VFC = (_props) => {
+    return (
+        <div
+            className="pb-status-bar"
+            role="status"
+            aria-live="off"
+            onContextMenu={(e): void => e.preventDefault()}
+        ></div>
+    );
+};
 
-export default connect()(StatusBar);
+export default StatusBar;
