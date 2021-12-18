db7f3be222f0b38b4ef296d7906e530adf39cb68
David Lechner <david@pybricks.com>
2021-12-18 17:35:39 -0600

    utils: convert ExternalLinkIcon to function

--- src/utils/ExternalLinkIcon.tsx
+++ src/utils/ExternalLinkIcon.tsx
@@ -8,17 +8,15 @@ import React from 'react';
 
 import './external-link.scss';
 
-class ExternalLinkIcon extends React.Component {
-    render(): JSX.Element {
-        return (
-            <span className="pb-external-link">
-                {' '}
-                <sup>
-                    <Icon icon="share" iconSize={12} />
-                </sup>
-            </span>
-        );
-    }
-}
+const ExternalLinkIcon: React.VFC = (_props) => {
+    return (
+        <span className="pb-external-link">
+            &nbsp;
+            <sup>
+                <Icon icon="share" iconSize={12} />
+            </sup>
+        </span>
+    );
+};
 
 export default ExternalLinkIcon;
