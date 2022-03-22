9d995ceeef03fbb0907b6b26f5d33662235e88ae
David Lechner <david@pybricks.com>
2022-03-21 22:34:28 -0500

    craco: add upper-case license

--- craco.config.js
+++ craco.config.js
@@ -84,6 +84,29 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.`;
 
+// https://github.com/blakeembrey/change-case
+const changeCaseLicense = `The MIT License (MIT)
+
+Copyright (c) 2014 Blake Embrey (hello@blakeembrey.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.`
+
 const licenseTextOverrides = {
     '@shopify/dates': shopifyLicense,
     '@shopify/decorators': shopifyLicense,
@@ -100,7 +123,8 @@ const licenseTextOverrides = {
     'hmac-drbg': fedorIndutnyLicense(2017),
     'miller-rabin': fedorIndutnyLicense(2014),
     'minimalistic-crypto-utils': fedorIndutnyLicense(2017),
-    gud: `MIT License
+    'upper-case': changeCaseLicense,
+    gud: `MIT License,
 
 Copyright (c)
 
