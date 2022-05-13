3fb98e6ae1199f046c5c6cb98798f304ddc3f4cb
David Lechner <david@pybricks.com>
2022-05-12 19:23:49 -0500

    ViewHeightSensor: hard-code style
    This style is not about looks, so hard-code it instead of using style sheets.

--- src/utils/ViewHeightSensor.tsx
+++ src/utils/ViewHeightSensor.tsx
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2022 The Pybricks Authors
+
 /* This is a hack for correctly sizing to view height on mobile when not running in fullscreen mode. */
 
 import { ResizeSensor } from '@blueprintjs/core';
@@ -15,7 +18,10 @@ function ViewHeightSensor(): JSX.Element {
                 );
             }}
         >
-            <div id="pb-vh" className="h-100 w-100 p-absolute" />
+            <div
+                id="pb-vh"
+                style={{ height: '100%', width: '100%', position: 'absolute' }}
+            />
         </ResizeSensor>
     );
 }
