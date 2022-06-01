691783e3f87f88d070f4f4962ce70eb69f6c3fc0
David Lechner <david@pybricks.com>
2022-06-01 16:43:25 -0500

    yarn: update linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
         'plugin:react/recommended',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
         'plugin:import/errors',
         'plugin:import/warnings',
