7810a8ee876984efac902621fa6a9f5b96d80d0d
David Lechner <david@pybricks.com>
2022-10-28 12:52:10 -0500

    components/hubPicker: use images instead of text

--- src/components/hubPicker/HubPicker.tsx
+++ src/components/hubPicker/HubPicker.tsx
@@ -1,25 +1,66 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2022 The Pybricks Authors
 
+import './hubPicker.scss';
 import { Radio, RadioGroup } from '@blueprintjs/core';
 import React from 'react';
 import { useHubPickerSelectedHub } from './hooks';
 import { Hub } from '.';
+
+type HubIconProps = {
+    url: URL;
+    label: string;
+};
+
+const HubIcon: React.VoidFunctionComponent<HubIconProps> = ({ url, label }) => {
+    return <img src={url.toString()} width={64} height={64} title={label} />;
+};
 
 export const HubPicker: React.VoidFunctionComponent = () => {
     const [selectedHub, setSelectedHub] = useHubPickerSelectedHub();
 
     return (
         <RadioGroup
+            className="pb-hub-picker"
             selectedValue={selectedHub}
             onChange={(e) => setSelectedHub(e.currentTarget.value as Hub)}
         >
-            <Radio value={Hub.Move}>BOOST Move Hub</Radio>
-            <Radio value={Hub.City}>City Hub</Radio>
-            <Radio value={Hub.Technic}>Technic Hub</Radio>
-            <Radio value={Hub.Prime}>SPIKE Prime Hub</Radio>
-            <Radio value={Hub.Essential}>SPIKE Essential Hub</Radio>
-            <Radio value={Hub.Inventor}>MINDSTORMS Robot Inventor Hub</Radio>
+            <Radio value={Hub.Move}>
+                <HubIcon
+                    url={new URL('@pybricks/images/hub-move.png', import.meta.url)}
+                    label="BOOST Move Hub"
+                />
+            </Radio>
+            <Radio value={Hub.City}>
+                <HubIcon
+                    url={new URL('@pybricks/images/hub-city.png', import.meta.url)}
+                    label="City Hub"
+                />
+            </Radio>
+            <Radio value={Hub.Technic}>
+                <HubIcon
+                    url={new URL('@pybricks/images/hub-technic.png', import.meta.url)}
+                    label="Technic Hub"
+                />
+            </Radio>
+            <Radio value={Hub.Prime}>
+                <HubIcon
+                    url={new URL('@pybricks/images/hub-prime.png', import.meta.url)}
+                    label="SPIKE Prime Hub"
+                />
+            </Radio>
+            <Radio value={Hub.Essential}>
+                <HubIcon
+                    url={new URL('@pybricks/images/hub-essential.png', import.meta.url)}
+                    label="SPIKE Essential Hub"
+                />
+            </Radio>
+            <Radio value={Hub.Inventor}>
+                <HubIcon
+                    url={new URL('@pybricks/images/hub-inventor.png', import.meta.url)}
+                    label="MINDSTORMS Robot Inventor Hub"
+                />
+            </Radio>
         </RadioGroup>
     );
 };
