cf99ab223d68260e0ac2b1f9f415618e3f5d7704
David Lechner <david@pybricks.com>
2022-11-18 16:40:24 -0600

    firmware/sagas: refactor firmwareFlashUsbDfu
    This allows the firmwareFlashUsbDfu to take an arbitrary binary blob
    instead of a Pybricks firmware.zip file. This will allow it to be used
    to restore the official LEGO firmware as well.

--- src/firmware/alerts/index.ts
+++ src/firmware/alerts/index.ts
@@ -2,7 +2,6 @@
 // Copyright (c) 2022 The Pybricks Authors
 
 import { dfuError } from './DfuError';
-import { firmwareMismatch } from './FirmwareMismatch';
 import { flashProgress } from './FlashProgress';
 import { noDfuHub } from './NoDfuHub';
 import { noDfuInterface } from './NoDfuInterface';
@@ -11,7 +10,6 @@ import { releaseButton } from './ReleaseButton';
 
 export default {
     dfuError,
-    firmwareMismatch,
     flashProgress,
     noDfuHub,
     noDfuInterface,
