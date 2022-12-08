936e142429870cafc68921261af07628d939ab70
David Lechner <david@pybricks.com>
2022-12-08 14:46:21 -0600

    app: remember docs page on refresh
    This persists the current docs page so that it is used on page reload
    of the app. This is mostly useful for development since users should
    not be refreshing the page.
    
    It also remembers the selected page between sessions, so new windows
    will open to the previous page.
    
    Checks are added to default back to the docs index page in case the
    "remembered" page is no longer valid.

---
+++ src/app/hooks.ts
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2022 The Pybricks Authors
+
+import { useEffect, useMemo } from 'react';
+import { useLocalStorage, useSessionStorage } from 'usehooks-ts';
+import { docsDefaultPage, docsPathPrefix, httpServerHeadersVersion } from './constants';
+
+const defaultPage = `${docsDefaultPage}?v$${httpServerHeadersVersion}`;
+
+export function useAppLastDocsPageSetting() {
+    const key = 'app.setting.lastDocsPage';
+
+    // last visited page is stored in local storage so that new windows get the
+    // last visited page
+    const [lastPageGlobalSetting, setLastPageGlobalSetting] = useLocalStorage(
+        key,
+        defaultPage,
+    );
+
+    // it is also stored in session storage in case two windows are open at the
+    // same time so that one window doesn't affect the other
+    const [lastPageSessionSetting, setLastPageSessionSetting] = useSessionStorage(
+        key,
+        lastPageGlobalSetting,
+    );
+
+    // mirror session storage value to local storage
+    useEffect(() => {
+        setLastPageGlobalSetting(lastPageSessionSetting);
+    }, [lastPageSessionSetting]);
+
+    // the way the docs control works, we only provide the initial page, then
+    // it manages navigation after that, so we only want the initial of this
+    // value when the app first starts
+    const initialDocsPage = useMemo(() => {
+        try {
+            const url = new URL(lastPageSessionSetting);
+
+            // in case someone is hacking the storage value directly
+            if (!url.pathname.startsWith(`/${docsPathPrefix}`)) {
+                throw new Error('invalid or outdated path');
+            }
+
+            return lastPageSessionSetting;
+        } catch {
+            return defaultPage;
+        }
+    }, []);
+
+    return { initialDocsPage, setLastDocsPage: setLastPageSessionSetting };
+}
