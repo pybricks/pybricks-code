6207fdfcb13e26b969b35c5958bcf2a2ef6c45ea
David Lechner <david@pybricks.com>
2023-02-16 11:18:09 -0600

    firmware/installPybricksDialog: add basic metadata check
    This will catch some errors. Mainly this is to ensure we show an error
    if someone tries to use, e.g. a NXT firmware file that isn't supported
    in Pybricks Code yet. Error message isn't great but we don't expect this
    to be hit often, if ever, by non-developers.

--- src/firmware/installPybricksDialog/index.ts
+++ src/firmware/installPybricksDialog/index.ts
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2022 The Pybricks Authors
+// Copyright (c) 2022-2023 The Pybricks Authors
+
+import { FirmwareMetadata, HubType } from '@pybricks/firmware';
+import * as semver from 'semver';
 
 const encoder = new TextEncoder();
 
@@ -17,4 +20,28 @@ export function validateHubName(hubName: string): boolean {
     // termination), so we can hard code the check here to allow notifying the
     // user earlier for better UX.
     return encoded.length < 16;
+}
+
+const supportHubs: readonly HubType[] = [
+    HubType.MoveHub,
+    HubType.CityHub,
+    HubType.TechnicHub,
+    HubType.PrimeHub,
+    HubType.EssentialHub,
+];
+
+export function validateMetadata(metadata: FirmwareMetadata) {
+    if (semver.satisfies(metadata['metadata-version'], '^1')) {
+        if (!supportHubs.includes(metadata['device-id'])) {
+            throw new Error(`"unsupported "device-id": ${metadata['device-id']}`);
+        }
+    } else if (semver.satisfies(metadata['metadata-version'], '^2')) {
+        if (!supportHubs.includes(metadata['device-id'])) {
+            throw new Error(`"unsupported "device-id": ${metadata['device-id']}`);
+        }
+    } else {
+        throw new Error(
+            `unsupported "metadata-version": "${metadata['metadata-version']}"`,
+        );
+    }
 }
