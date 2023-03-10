f53932a2d1fe478b3a9125b43a9e2deaf9307047
David Lechner <david@pybricks.com>
2023-03-10 17:27:54 -0600

    mpy/sagas: fix tests attempting to load .wasm via http
    Since we are using jsdom, the emscripten generated code doesn't use
    the node filesystem lookup for .wasm files. But then it falls back to
    node for file:// urls, but this doesn't work because node treats file:
    as a drive letter.
    
    We can work around this by modifying the node path.normalize() function
    to return the correct path.
    
    Fixes: https://github.com/pybricks/pybricks-code/issues/1584

--- src/mpy/sagas.test.ts
+++ src/mpy/sagas.test.ts
@@ -1,9 +1,43 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2020-2022 The Pybricks Authors
+// Copyright (c) 2020-2023 The Pybricks Authors
 
+import path from 'path';
 import { AsyncSaga } from '../../test';
 import { compile, didCompile, didFailToCompile } from './actions';
 import mpy from './sagas';
+
+const mpyCrossV5Wasm = require.resolve('@pybricks/mpy-cross-v5/build/mpy-cross.wasm');
+const mpyCrossV6Wasm = require.resolve(
+    '@pybricks/mpy-cross-v6/build/mpy-cross-v6.wasm',
+);
+
+beforeEach(() => {
+    // HACK: work around Emscripten + Webpack bugs
+    // Since we are using jsdom, emscripten thinks we are in a browser and
+    // sees that the path starts with file:// but just passes this to
+    // path.normalize() which treats file: as a windows-style drive prefix.
+    // Also, the webpack import.meta.url doesn't work correctly in the test
+    // environment either and returns a path relative to the directory where
+    // it was called rather than the node_modules/ directory. So we have to
+    // fake the normalization to get the correct path.
+    jest.spyOn(path, 'normalize').mockImplementation((p) => {
+        // NB: we can't call require.resolve() here because it would recursively
+        // call this function via path.normalize()!
+        if (p.endsWith('@pybricks/mpy-cross-v5/build/mpy-cross.wasm')) {
+            return mpyCrossV5Wasm;
+        }
+
+        if (p.endsWith('@pybricks/mpy-cross-v6/build/mpy-cross-v6.wasm')) {
+            return mpyCrossV6Wasm;
+        }
+
+        return p;
+    });
+});
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
 
 test('compiler works', async () => {
     const saga = new AsyncSaga(mpy);
