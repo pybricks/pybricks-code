c03e5782f813efebeca6a2200ab4a70696fbbd0e
David Lechner <david@pybricks.com>
2023-11-11 14:55:58 -0600

    setupTests: fix import for jest-dom v6
    Fixes breaking change: https://github.com/testing-library/jest-dom/releases/tag/v6.0.0

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,7 +5,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import 'navigator.locks';
 import crypto from 'crypto';
 import { inspect } from 'util';
