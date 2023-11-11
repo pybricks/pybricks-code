61efd12f3ce86459dfa1e4c345b38875caaf344f
David Lechner <david@pybricks.com>
2023-11-11 14:38:15 -0600

    fix type errors for react-splitter-layout
    Upgrading to `@types/react` > 18.2.21 causes the following errors:
    
    ```
    src/app/App.tsx:202:22 - error TS2786: 'SplitterLayout' cannot be used as a JSX component.
      Its instance type 'SplitterLayout' is not a valid JSX element.
        The types returned by 'render()' are incompatible between these types.
          Type 'import("/home/david/work/pybricks/pybricks-code/node_modules/@types/react-dom/node_modules/@types/react/ts5.0/index").ReactNode' is not assignable to type 'React.ReactNode'.
            Type 'ReactElement<any, string | JSXElementConstructor<any>>' is not assignable to type 'ReactNode'.
              Property 'children' is missing in type 'ReactElement<any, string | JSXElementConstructor<any>>' but required in type 'ReactPortal'.
    
    202                     <SplitterLayout
                             ~~~~~~~~~~~~~~
    
      node_modules/@types/react/ts5.0/index.d.ts:175:9
        175         children: ReactNode;
                    ~~~~~~~~
        'children' is declared here.
    
    src/app/App.tsx:212:26 - error TS2786: 'SplitterLayout' cannot be used as a JSX component.
      Its instance type 'SplitterLayout' is not a valid JSX element.
    
    212                         <SplitterLayout
                                 ~~~~~~~~~~~~~~
    
    Found 2 errors in the same file, starting at: src/app/App.tsx:202
    ```
    
    https://arethetypeswrong.github.io/?p=react-splitter-layout%404.0.0
    
    provided the hint that we need `export =` instead of `export default`.

---
+++ src/react-splitter-layout.d.ts
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2023 The Pybricks Authors
+
+declare module 'react-splitter-layout' {
+    export = class SplitterLayout extends React.PureComponent<SplitterLayoutProps> {};
+}
