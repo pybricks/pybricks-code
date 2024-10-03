d533b32036abb31a3e5fa77600790ef0a9073949
Attila Farago <attila.farago.hu@gmail.com>
2024-10-03 09:58:48 +0200

    improve: color sensor to show color only if valid

--- src/hubcenter/icons/ColorSensorIcon.tsx
+++ src/hubcenter/icons/ColorSensorIcon.tsx
@@ -8,33 +8,39 @@ const ColorSensorIconComponent: React.FunctionComponent<DeviceIconProps> = ({
     devEntry,
     portData,
 }) => {
-    const h = parseInt(portData?.dataMap?.get('h') ?? '');
+    const h = portData?.dataMap?.get('h')
+        ? parseInt(portData?.dataMap?.get('h') || '')
+        : undefined;
     const s = portData?.dataMap?.get('s');
     const v = portData?.dataMap?.get('v');
     const bgcolor = `hsl(${h}, ${s}, ${v})`;
 
     return (
         <div className="pb-device-icon">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 48 48"
-                className="color-eye"
-            >
-                <defs>
-                    <filter id="blur-filter">
-                        <feGaussianBlur in="SourceGraphic" stdDeviation="2" />
-                    </filter>
-                </defs>
-                <g>
-                    <circle
-                        fill={bgcolor}
-                        cx="24"
-                        cy="24"
-                        r="20"
-                        filter="url(#blur-filter)"
-                    />
-                </g>
-            </svg>
+            {h === undefined || s === undefined || v === undefined ? (
+                <></>
+            ) : (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 48 48"
+                    className="color-eye"
+                >
+                    <defs>
+                        <filter id="blur-filter">
+                            <feGaussianBlur in="SourceGraphic" stdDeviation="2" />
+                        </filter>
+                    </defs>
+                    <g>
+                        <circle
+                            fill={bgcolor}
+                            cx="24"
+                            cy="24"
+                            r="20"
+                            filter="url(#blur-filter)"
+                        />
+                    </g>
+                </svg>
+            )}
 
             <img src={devEntry?.icon} alt={devEntry?.name} className="icon" />
         </div>
