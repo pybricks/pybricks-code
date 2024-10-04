7d2342935c369b825a15177e4f092f7c630c1e89
Attila Farago <attila.farago.hu@gmail.com>
2024-10-04 23:26:44 +0200

    feat: close dialog on disconnect or program stop

--- src/hubcenter/reducers.ts
+++ src/hubcenter/reducers.ts
@@ -2,6 +2,9 @@
 // Copyright (c) 2022-2024 The Pybricks Authors
 
 import { Reducer, combineReducers } from '@reduxjs/toolkit';
+import { bleDidDisconnectPybricks } from '../ble/actions';
+import { didReceiveStatusReport } from '../ble-pybricks-service/actions';
+import { Status, statusToFlag } from '../ble-pybricks-service/protocol';
 import { hubcenterHideDialog, hubcenterShowDialog } from './actions';
 
 /** A list of open files in the order they should be displayed to the user. */
@@ -12,6 +15,19 @@ const showDialog: Reducer<boolean> = (state = false, action) => {
 
     if (hubcenterHideDialog.matches(action)) {
         return false;
+    }
+
+    if (bleDidDisconnectPybricks.matches(action)) {
+        return false;
+    }
+
+    if (didReceiveStatusReport.matches(action)) {
+        if (action.statusFlags & statusToFlag(Status.Shutdown)) {
+            return false;
+        }
+        if (!(action.statusFlags & statusToFlag(Status.UserProgramRunning))) {
+            return false;
+        }
     }
 
     return state;
