47418e6a788b8aab5a36f098cf9c1166b159cbc4
Attila Farago <attila.farago.hu@gmail.com>
2024-10-10 13:28:09 +0200

    fix: handleDownloadFile auth token

--- src/googleDrive/sagas.ts
+++ src/googleDrive/sagas.ts
@@ -24,7 +24,7 @@ function* handleDownloadFile(
     action: ReturnType<typeof googleDriveDownloadFile>,
 ): Generator {
     try {
-        const authToken = getOauthToken;
+        const authToken = getOauthToken();
 
         defined(authToken);
 
