b6cf5e8491a29de9f55c9b21f99aa64f94282f90
David Lechner <david@lechnology.com>
2026-01-25 22:03:25 +0000

    usb: implement AlreadyInUse alert
    Add an alert that notifies the user when the USB device is already in
    use by another application.
    
    Closes: https://github.com/pybricks/support/issues/2373

--- src/usb/alerts/index.ts
+++ src/usb/alerts/index.ts
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2025 The Pybricks Authors
+// Copyright (c) 2025-2026 The Pybricks Authors
 
+import { alreadyInUse } from './AlreadyInUse';
 import { newPybricksProfile } from './NewPybricksProfile';
 import { noWebUsb } from './NoWebUsb';
 import { oldFirmware } from './OldFirmware';
 
 // gathers all of the alert creation functions for passing up to the top level
 export default {
+    alreadyInUse,
     newPybricksProfile,
     noWebUsb,
     oldFirmware,
