c49df2b81f160cae17e68d8e1a40346f0098c7f7
Laurens Valk <laurens@pybricks.com>
2026-03-25 14:59:03 +0100

    scripts/serve: Optionally serve over https.
    Allows local testing on mobile devices with Bluetooth, USB, and camera suppport.

--- scripts/serve.py
+++ scripts/serve.py
@@ -6,9 +6,11 @@ Serve the build directory using builtin Python web server.
 
 import http.server
 import pathlib
-import socketserver
+import ssl
 
 BUILD_DIR = (pathlib.Path(__file__).parent.parent / "build").resolve()
+CERT_FILE = BUILD_DIR / "cert.pem"
+KEY_FILE = BUILD_DIR / "key.pem"
 PORT = 8000
 
 
@@ -17,10 +19,10 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self,
         request: bytes,
         client_address: tuple[str, int],
-        server: socketserver.BaseServer,
+        server: http.server.HTTPServer,
         directory: str | None = ...
     ) -> None:
-        super().__init__(request, client_address, server, directory=BUILD_DIR)
+        super().__init__(request, client_address, server, directory=str(BUILD_DIR))
 
     def end_headers(self) -> None:
         # custom headers needed for some web API features
@@ -29,6 +31,14 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         return super().end_headers()
 
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"serving at http://0.0.0.0:{PORT}")
-    httpd.serve_forever()
+httpd = http.server.HTTPServer(("", PORT), Handler)
+
+if CERT_FILE.exists() and KEY_FILE.exists():
+    ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ctx.load_cert_chain(certfile=CERT_FILE, keyfile=KEY_FILE)
+    httpd.socket = ctx.wrap_socket(httpd.socket, server_side=True)
+    print(f"serving at https://0.0.0.0:{PORT}")
+else:
+    print(f"cert/key not found, serving without TLS at http://0.0.0.0:{PORT}")
+
+httpd.serve_forever()
